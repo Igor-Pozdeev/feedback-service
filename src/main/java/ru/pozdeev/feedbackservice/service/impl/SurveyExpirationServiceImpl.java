e0fddec07b503d58681682454d05a8b186fa10b1
Igor <Sketch_90@mail.ru>
2026-02-23 18:46:44 +0300

    TASK-6: Конфигурация кафки и немного исправлений логов

--- src/main/java/ru/pozdeev/feedbackservice/service/impl/SurveyExpirationServiceImpl.java
+++ src/main/java/ru/pozdeev/feedbackservice/service/impl/SurveyExpirationServiceImpl.java
@@ -21,22 +21,22 @@ public class SurveyExpirationServiceImpl implements SurveyExpirationService {
     @Override
     @Transactional
     public int expireOldSurveys() {
-        log.info("Starting to expire old surveys...");
+        log.info("Starting to expire old surveys");
 
         int expiredBySentDate = surveyRepository.expireSentSurveys(
                 LocalDateTime.now().minusDays(feedbackProperties.getSentSurveyExpiryDays())
         );
-        log.info("Expired {} surveys based on sent date.", expiredBySentDate);
+        log.info("Expired {} surveys based on sent date", expiredBySentDate);
 
         int expiredByCreationDate = surveyRepository.expireUnsentSurveys(
                 LocalDateTime.now().minusDays(feedbackProperties.getUnsentSurveyExpiryDays())
         );
-        log.info("Expired {} surveys based on creation date.", expiredByCreationDate);
+        log.info("Expired {} surveys based on creation date", expiredByCreationDate);
 
         int expiredByCampaignEndDate = surveyRepository.expireSurveysWithFinishedCampaigns(
                 LocalDateTime.now()
         );
-        log.info("Expired {} surveys based on campaign end date.", expiredByCampaignEndDate);
+        log.info("Expired {} surveys based on campaign end date", expiredByCampaignEndDate);
 
         int totalExpired = expiredBySentDate + expiredByCreationDate + expiredByCampaignEndDate;
         log.info("Total expired surveys: {}", totalExpired);
